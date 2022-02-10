2a5d345246e199ed0131173a7fe35bb20ef3ac65
unknown <you@example.com>
2022-02-10 21:32:53 +0600

    Nothing serious

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 let resultField = document.getElementById('result');
 
-let buttons = document.getElementsByTagName('button');
-
-for (btn of buttons) {
+//Numbers calling
+let numbers = document.getElementsByClassName('number');
+for (btn of numbers) {
+  btn.addEventListener('click', function (event) {
+    let btnValue = event.target.innerText;
+    resultField.append(btnValue);
+  })
+}
+//Operators calling
+let operates = document.getElementsByClassName('operate');
+for (btn of operates) {
   btn.addEventListener('click', function (event) {
     let btnValue = event.target.innerText;
     resultField.append(btnValue);
   })
 }
 
+// AC button function
 let ac = document.getElementById('ac');
 ac.addEventListener('click', function () {
   resultField.innerText = '';
+})
+
+
+// = button function
+let equel = document.getElementById('equel');
+equel.addEventListener('click', function () {
+  console.log('= clicked');
+  resultField.innerText = parseFloat(resultField.innerText);
 })
 
