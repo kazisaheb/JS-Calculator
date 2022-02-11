eb806add079424f5640b66607ccda5f1598a2b9a
unknown <you@example.com>
2022-02-11 19:20:12 +0600

    Calculation workinggit add .

--- script.js
+++ script.js
@@ -8,26 +8,15 @@ for (btn of numbers) {
     resultField.append(btnValue);
   })
 }
-//Operators calling
-let operates = document.getElementsByClassName('operate');
-for (btn of operates) {
-  btn.addEventListener('click', function (event) {
-    let btnValue = event.target.innerText;
-    resultField.append(btnValue);
-  })
-}
+
+// = button function
+let equel = document.getElementById('equel');
+equel.addEventListener('click', function () {
+  resultField.innerText = eval(resultField.innerText);
+})
 
 // AC button function
 let ac = document.getElementById('ac');
 ac.addEventListener('click', function () {
   resultField.innerText = '';
 })
-
-
-// = button function
-let equel = document.getElementById('equel');
-equel.addEventListener('click', function () {
-  console.log('= clicked');
-  resultField.innerText = parseFloat(resultField.innerText);
-})
-
