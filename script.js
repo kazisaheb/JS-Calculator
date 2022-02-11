63273b1f0db7849e5976a78d5533dbd43a78028c
unknown <you@example.com>
2022-02-11 21:18:42 +0600

    Full functional

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 let resultField = document.getElementById('result');
-
+let inputField = document.getElementById('input');
 //Numbers calling
 let numbers = document.getElementsByClassName('number');
 for (btn of numbers) {
   btn.addEventListener('click', function (event) {
     let btnValue = event.target.innerText;
-    resultField.append(btnValue);
+    inputField.append(btnValue);
   })
 }
-
-// = button function
-let equel = document.getElementById('equel');
-equel.addEventListener('click', function () {
-  resultField.innerText = eval(resultField.innerText);
-})
-
 // AC button function
 let ac = document.getElementById('ac');
 ac.addEventListener('click', function () {
-  resultField.innerText = '';
+  inputField.innerText = '';
+  resultField.innerText = 0;
+})
+// CE button function
+let ce = document.getElementById('ce');
+ce.addEventListener('click', function () {
+  inputField.removeChild(inputField.lastChild);
+})
+// = button function
+let equel = document.getElementById('equel');
+equel.addEventListener('click', function () {
+  resultField.innerText = '= ' + eval(inputField.innerText);
 })
